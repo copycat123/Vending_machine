87fb96e7a6254507e807bec4f3540ffb9823e797
pushpinder <pushpinders555@gmail.com>
2019-05-23 00:04:18 +0530

    vending machine program

---
+++ Vending_machine.py
@@ -0,0 +1,22 @@
+sodas = ["pepsi", "cherry coke", "sprite"]
+chips = ["Doritos", "chetoz"]
+candy = ["Snickers", "M&M", "Twizz"]
+
+while True:
+    choice = input("Would you like a SODAS, some CHIPS , or a CANDY? ").lower()
+    try:
+
+        if choice == "sodas":
+            snack = sodas.pop()
+        elif choice == "chips":
+            snack = chips.pop()
+        elif choice == "candy":
+            snack = candy.pop()
+
+        else:
+            print("Sorry i didn't understand that.")
+            continue
+    except IndexError:
+        print("Sorry all out of {}".format(choice))
+    else:
+        print("Here is your {}: {}".format(choice, snack))
